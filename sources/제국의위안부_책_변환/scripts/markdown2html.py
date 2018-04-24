89ec94fa9d996cc78d86cfc3d9b0182ff68c7f0c
huinalam <huinalam@gmail.com>
2018-04-24 22:26:40 +0900

    마지막 페이지에서 다음 항목 삭제 & index.html로 접근하지 않도록 구현

--- sources/제국의위안부_책_변환/scripts/markdown2html.py
+++ sources/제국의위안부_책_변환/scripts/markdown2html.py
@@ -7,11 +7,11 @@ app = flask.Flask('my app')
 dir_path = 'data/markdown/'
 
 def create_home_address():
-    return '<a href="index.html">목록으로</a>'
+    return '<a href="/books/제국의위안부/">목록으로</a>'
 def create_prev_address(file_path, file_name):
-    return '<a href="{0}">이전으로 - {1}</a>'.format(file_path[:-2]+'html', file_name[6:-3].replace('-', ' '))
+    return '<a href="/books/제국의위안부/{0}">이전으로 - {1}</a>'.format(file_path[:-2]+'html', file_name[6:-3].replace('-', ' '))
 def create_next_address(file_path, file_name):
-    return '<a href="{0}">다음으로 - {1}</a>'.format(file_path[:-2]+'html', file_name[6:-3].replace('-', ' '))
+    return '<a href="/books/제국의위안부/{0}">다음으로 - {1}</a>'.format(file_path[:-2]+'html', file_name[6:-3].replace('-', ' '))
 
 if __name__ == '__main__':
     file_names = os.listdir(dir_path)
@@ -24,7 +24,7 @@ if __name__ == '__main__':
             md = markdown2.markdown(text)
         with app.app_context():
             prev_link = create_prev_address(file_zip[idx-1][1], file_zip[idx-1][1]) if idx>0 else ''
-            next_link = create_next_address(file_zip[idx+1][1], file_zip[idx+1][1]) if idx<file_count-1 else ''
+            next_link = create_next_address(file_zip[idx+1][1], file_zip[idx+1][1]) if idx<file_count-2 else ''
             rendered = render_template('book.html',
                                        title=file_name[:-2] if file_name.find('index')<0 else '제국의 위안부 E-Book', 
                                        home_link=create_home_address() if file_name.find('index')<0 else '',
